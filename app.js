5658852973ac3e6efa6f828420cc44be06b1bac7
Enrique Cornejo <enrique@cornejo.me>
2017-06-15 11:10:20 +0200

    Added listener implementation

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var antlr4 = require('antlr4');
 var WCPSLexer = require('./parsers/WCPSLexer');
 var WCPSParser = require('./parsers/WCPSParser');
-var WCPSListener = require('./parsers/WCPSListener');
-var WCPSVisitor = require('./parsers/WCPSVisitor');
-
+var QueryWCPSListener = require('./parsers/QueryWCPSListener').QueryWCPSListener;
 
 const inputQuery = 'for c in (AvgLandTemp) return 1';
 const chars = new antlr4.InputStream(inputQuery);
@@ -11,6 +9,7 @@ const lexer = new WCPSLexer.WCPSLexer(chars);
 const tokens = new antlr4.CommonTokenStream(lexer);
 const parser = new WCPSParser.WCPSParser(tokens);
 parser.buildParseTrees = true;
-// const tree = parser.file();
 const tree = parser.wcpsQuery();
-console.log(tree.children);
+const qListener = new QueryWCPSListener();
+antlr4.tree.ParseTreeWalker.DEFAULT.walk(qListener, tree);
+console.log("Fin");
---
+++ parsers/QueryWCPSListener.js
@@ -0,0 +1,23 @@
+var antlr4 = require('antlr4');
+var WCPSLexer = require('./WCPSLexer');
+var WCPSParser = require('./WCPSParser');
+var WCPSListener = require('./WCPSListener').WCPSListener;
+
+
+
+QueryWCPSListener = function(res) {
+    this.Res = res;    
+    WCPSListener.call(this); // inherit default listener
+    return this;
+};
+
+// inherits the default listener
+QueryWCPSListener.prototype = Object.create(WCPSListener.prototype);
+QueryWCPSListener.prototype.constructor = QueryWCPSListener;
+
+// override default listener behavior
+QueryWCPSListener.prototype.enterForClauseLabel = function(ctx) {
+    console.log("FOR CLAUSE");
+};
+
+exports.QueryWCPSListener = QueryWCPSListener;
